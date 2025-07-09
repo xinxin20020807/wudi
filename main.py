3c060d1e88d87dfbd9a0e9d8bb574304c59802f3
keepalived <13317025+keepalived_0807@user.noreply.gitee.com>
2025-07-09 10:28:28 +0800

    anyway

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get(
+    "/",
+    response_class=HTMLResponse,
+)
+def index(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "title": "首页", "message": "欢迎访问 FastAPI 网站!"},
+    )
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
